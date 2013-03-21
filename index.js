c8c3ecdb11d8104d2111861f51ecca576d53d7b4
Bernhard K. Weisshuhn <bkw@codingforce.com>
2013-03-21 16:42:19 +0100

    refactor bridge and light state stuff info separate libs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var join = require('path').join
-  , Bulb = require(join(__dirname, 'lib', 'bulb'))
+  , Bridge = require(join(__dirname, 'lib', 'bridge'))
   , async = require('async')
   ;
 
@@ -11,10 +11,12 @@ function wait(howLong, cb) {
     setTimeout(cb, howLong);
 }
 
-new Bulb('10.11.100.13', 'newdeveloper').on('ready', function (bulb) {
-    var tasks = [];
+new Bridge('10.11.100.13', 'newdeveloper').on('ready', function (bridge) {
+    var tasks = [],
+        bulb = bridge.getBulb(3);
+    console.log(bulb);
     process.argv.slice(2).forEach(function (col) {
-        tasks.push(async.apply(bulb.setColor.bind(bulb), 3, col, 5));
+        tasks.push(async.apply(bulb.setColor.bind(bulb), col, 5));
         tasks.push(async.apply(wait, 500));
     });
 

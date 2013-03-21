31af863463cbd51ba6cadad23b2801428ed5ea96
Bernhard K. Weisshuhn <bkw@codingforce.com>
2013-03-21 13:53:47 +0100

    add tiny demo program

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+#!/usr/bin/env node
 'use strict';
 
 var join = require('path').join
   , Bulb = require(join(__dirname, 'lib', 'bulb'))
-  , bulb = new Bulb('10.11.100.13', 'newdeveloper')
+  , async = require('async')
   ;
 
-bulb.on('ready', function (lights) {
-    console.dir(lights);
+
+function wait(howLong, cb) {
+    setTimeout(cb, howLong);
+}
+
+new Bulb('10.11.100.13', 'newdeveloper').on('ready', function (bulb) {
+    var tasks = [];
+    process.argv.slice(2).forEach(function (col) {
+        tasks.push(async.apply(bulb.setColor.bind(bulb), 3, col, 5));
+        tasks.push(async.apply(wait, 500));
+    });
+
+    async.series(
+        tasks,
+        function (err, res) {
+            if (err) {
+                throw new Error(err);
+            }
+        }
+    );
 });
 
